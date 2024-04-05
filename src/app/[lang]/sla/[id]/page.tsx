143ebfa9fa4a741caf424fb4a85862d09fc1015d
SamratArya <vishwaking234@gmail.com>
2024-04-05 18:26:54 +0530

    Implemented a repeater module in add new sla and further working on it.

--- src/app/[lang]/sla/[id]/page.tsx
+++ src/app/[lang]/sla/[id]/page.tsx
@@ -5,12 +5,14 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { useParams } from 'next/navigation'
 
+import type { BoxProps, CardContentProps, GridProps } from '@mui/material'
 import {
   Box,
   Button,
   Card,
   CardContent,
   CardHeader,
+  Collapse,
   Divider,
   FormControl,
   Grid,
@@ -27,6 +29,8 @@ import type { SwitchProps } from '@mui/material/Switch'
 import Switch from '@mui/material/Switch'
 
 // Type Imports
+import Repeater from '../../../../@core/components/repeater'
+
 import type { Locale } from '@/configs/i18n'
 import { getLocalizedUrl } from '@/utils/i18n'
 
@@ -78,6 +82,50 @@ const SLASwitch = styled((props: SwitchProps) => (
   }
 }))
 
+const RepeatingContent = styled(Grid)<GridProps>(({ theme }) => ({
+  paddingRight: 0,
+  display: 'flex',
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  border: `1px solid ${theme.palette.divider}`,
+  '& .col-title': {
+    top: '-2.375rem',
+    position: 'absolute'
+  },
+  [theme.breakpoints.down('md')]: {
+    '& .col-title': {
+      top: '0',
+      position: 'relative'
+    }
+  }
+}))
+
+const RepeaterWrapper = styled(CardContent)<CardContentProps>(({ theme }) => ({
+  padding: theme.spacing(16, 10, 10),
+  '& .repeater-wrapper + .repeater-wrapper': {
+    marginTop: theme.spacing(16)
+  },
+  [theme.breakpoints.down('md')]: {
+    paddingTop: theme.spacing(10)
+  },
+  [theme.breakpoints.down('sm')]: {
+    padding: theme.spacing(6)
+  }
+}))
+
+const InvoiceAction = styled(Box)<BoxProps>(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  padding: theme.spacing(2, 1)
+}))
+
+interface SectionValues {
+  sectionOneValue: string
+  sectionTwoValue: string
+  sectionThreeValue: string
+}
+
 const AddNewSLA = () => {
   const { lang: locale } = useParams()
 
@@ -115,12 +163,92 @@ const AddNewSLA = () => {
   const [urgentResolutionReplySelect, setUrgentResolutionReplySelect] = useState('Hours')
 
   const [operationalHoursSelect, setOperationalHoursSelect] = useState('Calendar Hours')
+  const [count, setCount] = useState<number>(1)
+
+  const [sectionValues, setSectionValues] = useState<SectionValues[]>([
+    { sectionOneValue: '', sectionTwoValue: '', sectionThreeValue: '' }
+  ])
 
   const handleChange = () => {
     setChecked(prev => !prev)
   }
 
   const checkedMessage = checked ? 'Enabled' : 'Disabled'
+
+  const deleteForm = () => {
+    setCount(count - 1)
+  }
+
+  const SectionOneOptions = [
+    { value: '', label: 'Select' },
+    { value: 'department', label: 'Department' },
+    { value: 'channel', label: 'Channel' },
+    { value: 'team', label: 'Team' },
+    { value: 'tags', label: 'Tags' }
+  ]
+
+  const SectionTwoOptions = [
+    { value: '', label: 'Select' },
+    { value: 'equals to', label: 'Equals To' },
+    { value: 'not equals to', label: 'Not Equals To' }
+  ]
+
+  const SectionThreeOptions = [
+    { value: '', label: 'Select' },
+    { value: 'test1', label: 'Test 1' },
+    { value: 'test2', label: 'Test 2' }
+  ]
+
+  const handleSectionOneChange = (e: React.ChangeEvent<{ value: string }>, index: number) => {
+    const { value } = e.target
+
+    setSectionValues(prevState => {
+      const updatedValues = [...prevState]
+
+      updatedValues[index] = { ...updatedValues[index], sectionOneValue: value as string }
+
+      return updatedValues
+    })
+  }
+
+  const handleSectionTwoChange = (e: React.ChangeEvent<{ value: string }>, index: number) => {
+    const { value } = e.target
+
+    setSectionValues(prevState => {
+      const updatedValues = [...prevState]
+
+      updatedValues[index] = { ...updatedValues[index], sectionTwoValue: value as string }
+
+      return updatedValues
+    })
+  }
+
+  const handleSectionThreeChange = (e: React.ChangeEvent<{ value: string }>, index: number) => {
+    const { value } = e.target
+
+    setSectionValues(prevState => {
+      const updatedValues = [...prevState]
+
+      updatedValues[index] = { ...updatedValues[index], sectionThreeValue: value as string }
+
+      return updatedValues
+    })
+  }
+
+  const handleAddNewItem = () => {
+    setCount(prevCount => prevCount + 1)
+    setSectionValues(prevState => [...prevState, { sectionOneValue: '', sectionTwoValue: '', sectionThreeValue: '' }])
+  }
+
+  const handleAddSubNewItem = () => {
+    // setSubCount(prevCount => prevCount + 1)
+  }
+
+  const resetForm = () => {
+    const defaultSectionValues = { sectionOneValue: '', sectionTwoValue: '', sectionThreeValue: '' }
+
+    setSectionValues([defaultSectionValues])
+  }
 
   return (
     <>
@@ -188,6 +316,138 @@ const AddNewSLA = () => {
               </Typography>
             </Box>
           </Box>
+
+          <Divider />
+
+          <RepeaterWrapper>
+            <Repeater count={count}>
+              {(i: number) => {
+                const Tag = i === 0 ? Box : Collapse
+
+                return (
+                  <>
+                    {i > 0 && count > 1 && (
+                      <Typography variant='h6' className='capitalize mt-3 mb-5 bg-gray-200 w-12 p-2'>
+                        AND
+                      </Typography>
+                    )}
+                    <Tag key={i} className='repeater-wrapper' {...(i !== 0 ? { in: true } : {})}>
+                      <Grid container spacing={2}>
+                        <RepeatingContent item xs={12}>
+                          {i >= 0 && (
+                            <Grid item xs={1}>
+                              <Button variant='text' className='mt-5' onClick={handleAddSubNewItem}>
+                                <i className='tabler-plus' />
+                                ADD
+                              </Button>
+                            </Grid>
+                          )}
+
+                          <Grid item xs={3} className='m-5'>
+                            <Box display='flex' flexDirection='column'>
+                              <FormControl className='flex-grow' size='small'>
+                                <InputLabel id={`demo-select-small-label-${count}-${i}`}>Select</InputLabel>
+                                <Select
+                                  labelId={`demo-select-small-label-${count}-${i}`}
+                                  id={`condition-${count}-${i}`}
+                                  label='Select'
+                                  placeholder='Select'
+                                  value={sectionValues[i]?.sectionOneValue}
+                                  onChange={e => handleSectionOneChange(e, i)}
+                                >
+                                  {SectionOneOptions.map(option => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                      {option.label}
+                                    </MenuItem>
+                                  ))}
+                                </Select>
+                              </FormControl>
+                            </Box>
+                          </Grid>
+
+                          {sectionValues[i]?.sectionOneValue && (
+                            <Grid item xs={3} className='m-5'>
+                              <Box display='flex' flexDirection='column'>
+                                <FormControl className='flex-grow' size='small'>
+                                  <InputLabel id={`demo-select-small-label-${count}-${i}`}>Select</InputLabel>
+                                  <Select
+                                    labelId={`demo-select-small-label-${count}-${i}`}
+                                    id={`operator-${count}-${i}`}
+                                    label='Select'
+                                    placeholder='Select'
+                                    value={sectionValues[i]?.sectionTwoValue}
+                                    onChange={e => handleSectionTwoChange(e, i)}
+                                  >
+                                    {SectionTwoOptions.map(option => (
+                                      <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                      </MenuItem>
+                                    ))}
+                                  </Select>
+                                </FormControl>
+                              </Box>
+                            </Grid>
+                          )}
+
+                          {sectionValues[i]?.sectionOneValue && sectionValues[i]?.sectionTwoValue && (
+                            <Grid item xs={3} className='m-5'>
+                              <Box display='flex' flexDirection='column'>
+                                <FormControl className='flex-grow' size='small'>
+                                  <InputLabel id={`demo-select-small-label-${count}-${i}`}>Select</InputLabel>
+                                  <Select
+                                    labelId={`demo-select-small-label-${count}-${i}`}
+                                    id={`source-${count}-${i}`}
+                                    label='Select'
+                                    placeholder='Select'
+                                    value={sectionValues[i]?.sectionThreeValue}
+                                    onChange={e => handleSectionThreeChange(e, i)}
+                                  >
+                                    {SectionThreeOptions.map(option => (
+                                      <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                      </MenuItem>
+                                    ))}
+                                  </Select>
+                                </FormControl>
+                              </Box>
+                            </Grid>
+                          )}
+
+                          {i !== 0 && (
+                            <Box className='absolute top-0 right-0'>
+                              <InvoiceAction>
+                                <IconButton size='small' onClick={deleteForm}>
+                                  <i className='tabler-x border-solid text-[20px] text-textSecondary' />
+                                </IconButton>
+                              </InvoiceAction>
+                            </Box>
+                          )}
+
+                          {i === 0 && (
+                            <Box className='absolute top-0 right-0'>
+                              <InvoiceAction>
+                                <IconButton size='small' onClick={resetForm}>
+                                  <i className='tabler-refresh border-solid text-[20px] text-textSecondary' />
+                                </IconButton>
+                              </InvoiceAction>
+                            </Box>
+                          )}
+                        </RepeatingContent>
+                      </Grid>
+                    </Tag>
+                  </>
+                )
+              }}
+            </Repeater>
+
+            <Grid container className='mt-4'>
+              <Grid item xs={12}>
+                <Button variant='contained' onClick={handleAddNewItem}>
+                  AND
+                </Button>
+              </Grid>
+            </Grid>
+          </RepeaterWrapper>
 
           <Divider />
 
