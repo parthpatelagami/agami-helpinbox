9d4c6664489115d7fa2bd09b316b4e7afaf0eaec
parthpatelagami <parth.patel@agami-tech.com>
2024-04-08 18:13:57 +0530

    ThemeConfig Change

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -60,7 +60,7 @@ const themeConfig: Config = {
   skin: 'default', // 'default', 'bordered'
   semiDark: false, // true, false
   layout: 'vertical', // 'vertical', 'collapsed', 'horizontal'
-  layoutPadding: 24, // Common padding for header, content, footer layout components (in px)
+  layoutPadding: 10, // Common padding for header, content, footer layout components (in px)
   compactContentWidth: 1440, // in px
   navbar: {
     type: 'fixed', // 'fixed', 'static'
