c739f694ac8c0753e9ee063815bfe6fe9a2c5b0d
Aadil Mangera <aadil.mangera@agami-tech.com>
2024-04-08 18:06:08 +0530

    Style issue in Loader

--- src/components/LoaderProvider.tsx
+++ src/components/LoaderProvider.tsx
@@ -6,30 +6,27 @@ import themeConfig from '@/configs/themeConfig'
 import { useTheme } from '@mui/material'
 const LoaderProvider = ({ children }: ChildrenType) => {
   const theme = useTheme()
+  const customStyles = `
+    #nprogress {
+      pointer-events: none;
+    }
+    #nprogress .bar {
+      left: 0;
+      top: 0;
+      height: 3px;
+      width: 100%;
+      z-index: 2000;
+      position: fixed;
+      background-color: ${theme.palette.primary.main};
+    }
+  `
   return (
     <>
       {children}
-      {themeConfig.routingLoader && (
-        <ProgressBar
-          shallowRouting
-          style={JSON.stringify({
-            '#nprogress': {
-              pointerEvents: 'none'
-            },
-            '& .bar': {
-              left: 0,
-              top: 0,
-              height: 3,
-              width: '100%',
-              zIndex: 2000,
-              position: 'fixed',
-              backgroundColor: theme.palette.primary.main
-            }
-          })}
-        />
-      )}
+      {themeConfig.routingLoader && <ProgressBar shallowRouting style={customStyles} />}
     </>
   )
 }
+//backgroundColor: theme.palette.primary.main
 
 export default LoaderProvider
