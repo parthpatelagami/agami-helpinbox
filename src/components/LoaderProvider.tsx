4dc14f9a2542b7d7a671223e001166c946660b2b
Aadil Mangera <aadil.mangera@agami-tech.com>
2024-04-08 17:38:07 +0530

    Changed loader styling

--- src/components/LoaderProvider.tsx
+++ src/components/LoaderProvider.tsx
@@ -3,12 +3,31 @@
 import { ChildrenType } from '@/@core/types'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import themeConfig from '@/configs/themeConfig'
-
+import { useTheme } from '@mui/material'
 const LoaderProvider = ({ children }: ChildrenType) => {
+  const theme = useTheme()
   return (
     <>
       {children}
-      {themeConfig.routingLoader && <ProgressBar height='1px' color='red' shallowRouting />}
+      {themeConfig.routingLoader && (
+        <ProgressBar
+          shallowRouting
+          style={JSON.stringify({
+            '#nprogress': {
+              pointerEvents: 'none'
+            },
+            '& .bar': {
+              left: 0,
+              top: 0,
+              height: 3,
+              width: '100%',
+              zIndex: 2000,
+              position: 'fixed',
+              backgroundColor: theme.palette.primary.main
+            }
+          })}
+        />
+      )}
     </>
   )
 }
