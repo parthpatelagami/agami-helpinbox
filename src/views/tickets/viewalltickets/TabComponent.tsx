71dd8aa48c3c2cf6dc084bfd68ad407dfe32fd31
mustafa.dahodwala <mustafa.dahodwala@agami-tech.com>
2024-04-12 18:05:07 +0530

    Added Reply page.

--- src/views/tickets/viewalltickets/TabComponent.tsx
+++ src/views/tickets/viewalltickets/TabComponent.tsx
@@ -1,18 +1,17 @@
 import { TabPanel } from '@mui/lab'
 import TableServerSide from './TableServerSide'
 import { data } from './ViewAllTicketData'
+import ReplyPage from '../reply/replypage1/ReplyPage'
 const TabComponent = (props: any) => {
   return (
     <>
       <TabPanel value='1'>
         <TableServerSide tableData={data} />
       </TabPanel>
-      {/* {props.incremntTab.length > 1 && props.incremntTab.map((tabValue: number) => (
-                <TabPanel value={tabValue.toString()}>
-                    {tabValue !== 1 && <ReplyPage />}
-                </TabPanel>
-            )
-            )} */}
+      {props.incremntTab.length > 1 &&
+        props.incremntTab.map((tabValue: number) => (
+          <TabPanel value={tabValue.toString()}>{tabValue !== 1 && <ReplyPage />}</TabPanel>
+        ))}
 
       {/* <TabPanel value="2">
                 <FormLayoutsBasic />
